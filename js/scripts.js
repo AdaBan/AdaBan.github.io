7fb6e1ed20429b1603ea01dfcbd0afaa48831aae
Michał Gacek <gacek.michael@outlook.com>
2017-04-04 21:39:05 +0200

    Some improvements

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,20 @@
-function changeColor(button, color) {	
-	$(button).click(function() {
-		$("#color-buttons button").removeClass("selected");	
-		$(".main-color").css("background", color);
-		$(button).addClass("selected");
-	});
+function changeColor(button, color) {   
+    document.getElementById(button).addEventListener('click', function() {
+        var buttons = document.querySelectorAll('#color-buttons button'),
+            mainColorButtons = document.getElementsByClassName('main-color');
+        
+        for (var i = 0, size = buttons.length; i < size; i++) {
+            buttons[i].classList.remove('selected');
+        }
+      
+        for (var i = 0, size = mainColorButtons.length; i < size; i++) {
+            mainColorButtons[i].style.background = color;
+        }
+      
+        this.classList.add('selected');
+    });
 }
-changeColor("#btn-hazelnut","#d1af94");
-changeColor("#btn-blue","#22264b");
-changeColor("#btn-olive","#AB8F46");
+
+changeColor("btn-hazelnut", "#d1af94");
+changeColor("btn-blue", "#22264b");
+changeColor("btn-olive", "#AB8F46");
